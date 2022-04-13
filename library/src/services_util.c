6335a1f33378a0cf4eb4aeb895a9740db71b57cb
Tatiana Leon <Tatiana.Leon@digi.com>
2022-04-13 14:15:57 +0200

    library: services: add functions to execute shell commands
    https://onedigi.atlassian.net/browse/DEL-7899
    
    Signed-off-by: Tatiana Leon <Tatiana.Leon@digi.com>

--- library/src/services_util.c
+++ library/src/services_util.c
@@ -17,12 +17,18 @@
  * ===========================================================================
  */
 
-#include <errno.h>
-#include <unistd.h>
+#include <alloca.h>
 #include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include "services_util.h"
 #include "cc_logging.h"
@@ -107,6 +113,27 @@
 
 #define RESP_END_OF_MESSAGE	0
 #define RESP_ERROR			1
+
+#define concat_va_list(arg) __extension__({		\
+	__typeof__(arg) *_l;				\
+	va_list _ap;					\
+	int _n;						\
+							\
+	_n = 0;						\
+	va_start(_ap, arg);				\
+	do { ++_n;					\
+	} while (va_arg(_ap, __typeof__(arg)));		\
+	va_end(_ap);					\
+	_l = alloca((_n + 1) * sizeof(*_l));		\
+							\
+	_n = 0;						\
+	_l[0] = arg;					\
+	va_start(_ap, arg);				\
+	do { _l[++_n] = va_arg(_ap, __typeof__(arg));	\
+	} while (_l[_n]);				\
+	va_end(_ap);					\
+	_l;						\
+})
 
 
 const char *to_send_error_msg(ccapi_send_error_t error) {
@@ -390,4 +417,143 @@ int read_blob(int fd, void **buffer, size_t *length, struct timeval *timeout)
 int write_blob(int fd, const void *data, size_t data_length)
 {
 	return send_blob(fd, "b:", data, data_length );
+}
+
+int execute_cmd(const char *cmd, char **resp, int timeout)
+{
+	FILE *f_cmd, *f_out;
+	int bytes_available, rc;
+	pid_t pid;
+	char *timeout_str = NULL;
+
+	f_cmd = tmpfile();
+	if (!f_cmd) {
+		if (asprintf(resp, "Error executing '%s' (cmd tmpfile)", cmd) < 0)
+			log_error("Error executing '%s' (cmd tmpfile): %s, %d", cmd, strerror(errno), errno);
+		return 1;
+	}
+
+	if (write(fileno(f_cmd), cmd, strlen(cmd)) < 0
+		|| write(fileno(f_cmd), "\n", 1) < 1) {
+		if (errno != EAGAIN && errno != EWOULDBLOCK) {
+			fclose(f_cmd);
+			if (asprintf(resp, "Error executing '%s'", cmd)  < 0)
+				log_error("Error executing '%s': %s, %d", cmd, strerror(errno), errno);
+			return 1;
+		}
+	}
+
+	f_out = tmpfile();
+	if (!f_out) {
+		fclose(f_cmd);
+		if (asprintf(resp, "Error executing '%s' (out tmpfile)", cmd) < 0)
+			log_error("Error executing '%s' (out tmpfile): %s, %d", cmd, strerror(errno), errno);
+		return 1;
+	}
+
+	fseek(f_cmd, 0, SEEK_SET);
+	fseek(f_out, 0, SEEK_SET);
+
+	if (asprintf(&timeout_str, "%d", timeout) < 0)
+		timeout_str = NULL;
+
+	pid = process_exec_fd(fileno(f_cmd), fileno(f_out), fileno(f_out),
+				"timeout", timeout_str != NULL ? timeout_str : "0", "/bin/sh");
+	free(timeout_str);
+
+	rc = process_wait(pid);
+	if (rc != 0)
+		log_debug("%s: Failed to execute '%s' (%d)", __func__, cmd, rc);
+
+	fseek(f_out, 0, SEEK_SET);
+	ioctl(fileno(f_out), FIONREAD, &bytes_available);
+	if (bytes_available > 0) {
+		int read_rc = -1;
+
+		*resp = calloc(bytes_available + 1, sizeof(char));
+		read_rc = read(fileno(f_out), *resp, bytes_available);
+		if (read_rc < 0) {
+			rc = 1;
+			log_debug("%s: Failed to get response for %s (%d)", __func__, *resp, read_rc);
+		}
+	}
+
+	fclose(f_cmd);
+	fclose(f_out);
+
+	if (rc && !*resp) {
+		if (asprintf(resp, "Error executing '%s'", cmd) < 0)
+			log_error("Error executing '%s': %s, %d", cmd, strerror(errno), errno);
+	}
+
+	return rc;
+}
+
+pid_t _process_exec_fd(int infd, int outfd, int errfd, const char *cmd, ...)
+{
+	const char **cmd_list = NULL;
+	struct sigaction sa = { .sa_handler = SIG_DFL };
+	pid_t pid;
+	va_list argp;
+
+	va_start(argp, cmd);
+	cmd_list = concat_va_list(cmd);
+
+	pid = fork();
+	if (pid != 0)
+		return pid;
+
+	if (infd < 0)
+		infd = open("/dev/null", O_RDONLY);
+	fcntl(infd, F_SETFD, fcntl(infd, F_GETFD) & ~FD_CLOEXEC);
+	if (infd != STDIN_FILENO) {
+		dup2(infd, STDIN_FILENO);
+		if (infd != outfd && infd != errfd)
+			close(infd);
+	}
+
+	if (outfd < 0)
+		outfd = open("/dev/null", O_WRONLY);
+	fcntl(outfd, F_SETFD, fcntl(outfd, F_GETFD) & ~FD_CLOEXEC);
+	if (outfd != STDOUT_FILENO) {
+		dup2(outfd, STDOUT_FILENO);
+		if (outfd != errfd)
+			close(outfd);
+	}
+
+	if (errfd < 0)
+		errfd = open("/dev/null", O_WRONLY);
+	fcntl(errfd, F_SETFD, fcntl(errfd, F_GETFD) & ~FD_CLOEXEC);
+	if (errfd != STDERR_FILENO) {
+		dup2(errfd, STDERR_FILENO);
+		close(errfd);
+	}
+
+	sigaction(SIGPIPE, &sa, NULL);
+
+	execvp(cmd_list[0], (char * const *)cmd_list);
+
+	_exit(1);
+}
+
+int process_wait(pid_t pid)
+{
+	int status;
+	int ret;
+
+	if (pid <= 0)
+		return -256;
+
+	do {
+		ret = waitpid(pid, &status, 0);
+	} while (ret < 0 && errno == EINTR);
+	if (ret != pid)
+		return -257;
+
+	if (WIFEXITED(status)) {
+		return WEXITSTATUS(status);
+	} else if (WIFSIGNALED(status)) {
+		return -WTERMSIG(status);
+	} else
+		return -258;
 }
