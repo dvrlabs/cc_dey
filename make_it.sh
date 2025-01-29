ecd1a7b0c384e2dfeb9351e7f37e9ce7fb8c1c1c
DVR <dvrlabs.tv@gmail.com>
2025-01-29 10:25:10 -0500

    added secondary update info location

--- make_it.sh
+++ make_it.sh
@@ -1,3 +1,11 @@
+# installing the toolchain:
+# (ConnectCore 6+ QP SBC)
+# wget https://ftp1.digi.com/support/digiembeddedyocto/3.2/r3/sdk/ccimx6qpsbc/xwayland/dey-glibc-x86_64-dey-image-qt-xwayland-cortexa9t2hf-neon-ccimx6qpsbc-toolchain-3.2-r3.sh
+# chmod +x dey-glibc-x86_64-dey-image-qt-xwayland-cortexa9t2hf-neon-ccimx6qpsbc-toolchain-3.2-r3.sh
+# ./dey-glibc-x86_64-dey-image-qt-xwayland-cortexa9t2hf-neon-ccimx6qpsbc-toolchain-3.2-r3.sh
+
+
+
 rm app/cloud-connector 
 rm library/libcloudconnector.a
 source /opt/dey/3.2-r3/ccimx6qpsbc/environment-setup-cortexa9t2hf-neon-dey-linux-gnueabi
